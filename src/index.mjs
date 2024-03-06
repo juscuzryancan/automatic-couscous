4a211eb627eef925206afd4ee71511e488ed4bbc
Ryan Riley Puzon <rrgpuzon@gmail.com>
2024-03-05 20:49:21 -0800

    creates 404 and error handling middlewares

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,28 @@ import express  from "express";
 
 const app = express();
 
+app.get("/", (req, res, next) => {
+	res.send("THIS IS HOME")
+});
+
+// 404 Handler
+app.get('*', (req, res, next) => {
+	res.status(404).send({
+		error: '404 - Not Found', 
+		message: 'No route found for the requested URL'});
+});
+
+// error handling middleware
+app.use((error, req, res, next) => {
+	console.error('SERVER ERROR: ', error);
+	if(res.status < 400) res.status(500);
+	res.send({
+		error: error.message, 
+		name: error.name,
+		table: error.table
+	});
+})
+
 const { PORT = 8080 } = process.env;
 app.listen(PORT, () => {
 	console.log(`Server started on PORT: ${PORT}`);
