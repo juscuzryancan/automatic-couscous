8f329a0235130c7aa9f39d3f47bbe5778b94a00e
Ryan Riley Puzon <rrgpuzon@gmail.com>
2024-03-06 23:47:12 -0800

    creates connection listener or socketio

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,27 @@
 import express  from 'express';
 import morgan from 'morgan';
 import cors from 'cors'
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import path from 'node:path';
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+	console.log('a user connected');
+	socket.on('disconnect', () => {
+		console.log('user disconnected')
+	})
+})
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.get("/", (req, res, next) => {
-	res.send('THIS IS HOME');
+app.get('/', (req, res, next) => {
+	res.sendFile(path.join(new URL(import.meta.url).pathname, '..', 'public/index.html'));
 });
 
 // 404 Handler
@@ -32,6 +44,6 @@ app.use((error, req, res, next) => {
 })
 
 const { PORT = 8080 } = process.env;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	console.log(`Server started on PORT: ${PORT}`);
 })
