3008484965f7576edf83dc38a5ad6ff7580cb7b2
Ryan Riley Puzon <rrgpuzon@gmail.com>
2024-03-16 01:23:26 -0700

    adds routers into index

--- server/routers/auth.mjs
+++ server/routers/auth.mjs
@@ -1,12 +1,26 @@
 import { Router } from "express";
 import jwt from "jsonwebtoken";
+import { validate } from "../utils.mjs";
+import { loginSchema, registerSchema } from "../schema.mjs";
+import bcrypt from "bcrypt";
+import { createUser, getUserByUsername } from "../db/users.mjs";
 
 const { JWT_SECRET } = process.env;
 const router = Router();
 
-router.post("/login", (req, res, next) => {
+router.post("/login", validate(loginSchema), async (req, res, next) => {
+  const { username, password } = req.body;
   try {
-    const user = "to be filled in";
+    const user = await getUserByUsername(username);
+    if (!user) {
+      return res.status(401).send({ message: "Invalid Credentials" });
+    }
+
+    const match = bcrypt.compare(user.password, password);
+    if (!match) {
+      return res.status(401).send({ message: "Invalid Credentials" });
+    }
+
     const token = jwt.sign(user, JWT_SECRET, { expiresIn: "7d" });
     res.send(token);
   } catch (error) {
@@ -14,9 +28,10 @@ router.post("/login", (req, res, next) => {
   }
 });
 
-router.post("/register", (req, res, next) => {
+router.post("/register", validate(registerSchema), async (req, res, next) => {
   try {
-    const user = "to be filled in";
+    await createUser(req.body);
+    res.sendStatus(201);
   } catch (error) {
     next(error);
   }
